e458b6c3fb53c08607790d82fc91069a5c3a0bd9
Phillipp Schmedt <16028406+phillippschmedt@users.noreply.github.com>
2024-01-18 20:33:55 +0100

    Update MainPage.xaml.cs

--- SecureStorageIssue/MainPage.xaml.cs
+++ SecureStorageIssue/MainPage.xaml.cs
@@ -12,7 +12,7 @@ public partial class MainPage : ContentPage
 	private async void OnCounterClicked(object sender, EventArgs e)
 	{
 		var key = "TestKey";
-        // After restarting the app and pressing the button again we expect a value here
+        // After restarting the app and pressing the button again we expect readOldKey to be filled
 		// because we called SetAsync after the RemoveAll(), readOldKey will be null though.
         var readOldKey = await SecureStorage.Default.GetAsync(key); 
 		SecureStorage.RemoveAll();
